4a8b8acdb4d6907916c1ae35989a0ee632cda794
John Lee <jslee1992@gmail.com>
2019-04-25 17:53:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+function submitData(name, email){
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+
+  }
+
+  fetch('http://localhost:3000/users', configObj)
+    .then(resp => resp.json())
+    .then(function(json){
+      const body = document.querySelector('body')
+      body.innerHTML = json.id
+    })
+    .catch(function(error){
+      document.body.innerHTML = error.message
+      // message: message,
+      // code: '401'
+    })
+}
+//
+// submitData("steve", "steve@steve.com")
